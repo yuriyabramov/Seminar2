772e3d1a8caececa9aaef571c66d114124260c27
Yury_Lomatov <yury.lomatov@yandex.ru>
2023-04-21 11:43:14 +0300

    added Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23, например: 14 -> нет; 46 -> нет.
+
+int varA;
+while(true)
+{
+    Console.Write("Введите число: ");
+    if(int.TryParse(Console.ReadLine(), out varA))
+    break;
+    Console.WriteLine("false");
+}
+if(varA % 7 == 0 && varA % 23 == 0)
+{
+    Console.WriteLine($"{varA} -> ДА");
+}
+else
+{
+    Console.WriteLine($"{varA} -> НЕТ");
+}
